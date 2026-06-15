1f005d7bfd264148d28dec332783a96d651adb76
a.fayaz <a.fayaz@safarbooking.com>
2026-06-15 14:46:34 +0330

    Fix the issue metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,11 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
+const { withExpoRouter } = require("expo-router/metro");
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, {
-    input: "./src/global.css",
-});
+module.exports = withExpoRouter(
+    withNativeWind(config, {
+        input: "./src/global.css",
+    })
+);
