134ebc894939f0eb9547fda200b60202083b3439
a.fayaz <a.fayaz@safarbooking.com>
2026-06-16 10:12:11 +0330

    Revert "Fix the issue metro.config.js"
    This reverts commit 1f005d7bfd264148d28dec332783a96d651adb76.

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,8 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
-const { withExpoRouter } = require("expo-router/metro");
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withExpoRouter(
-    withNativeWind(config, {
-        input: "./src/global.css",
-    })
-);
+module.exports = withNativeWind(config, {
+    input: "./src/global.css",
+});
